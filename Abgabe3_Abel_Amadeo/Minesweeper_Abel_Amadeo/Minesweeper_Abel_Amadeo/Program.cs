de35e0bae908eee94200fef88ac621ca96cd1472
Abel Solomon <abel.solomon@ksb-sg.ch>
2024-03-15 13:58:52 +0100

    Projekt Minesweeper erstellt, klassen hinzugefügt

---
+++ Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Minesweeper_Abel_Amadeo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/StartPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper_Abel_Amadeo
+{
+    internal class StartPage
+    {
+        //Deklaration
+        private Menu menu;
+
+        //Methoden
+        public void Show()
+        {
+
+        }
+
+    }
+}
