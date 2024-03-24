d0ba4766aab686a095bf48a3181f86c392b14a2c
Abel Solomon <abel.solomon@ksb-sg.ch>
2024-03-25 00:56:02 +0100

    Initialisieren Der Mienen fast fertig jedoch noch mit fehlern Drin

--- Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/Model.cs
+++ Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/Model.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Minesweeper_Abel_Amadeo
 {
-    internal class Model 
+    internal class Model
     {
         private GameState gameState;
         private Caretaker careTaker;
@@ -22,10 +22,10 @@ namespace Minesweeper_Abel_Amadeo
         private int width;
         private int height;
 
-        public Model(IDifficulty difficulty)
-        {
+        /* public Model(IDifficulty difficulty)
+         {
 
-        }
+         }*/
 
         public Model()
         {
@@ -35,7 +35,7 @@ namespace Minesweeper_Abel_Amadeo
         {
             Menu menu = new Menu();
             menu.selectDifficulty();
-            
+
         }
 
         public void CheckForWin()
@@ -45,45 +45,135 @@ namespace Minesweeper_Abel_Amadeo
 
         public void CheeckForLoss()
         {
-            
+
         }
+
 
         public void InitFieldsEasyMode()
         {
-            char[,] gameBoard = new char[8, 8];
+            Field[,] gameBoard = new Field[8, 8];
 
-            // Fülle das Spielbrett mit #
-            for (int column = 0; column < 8; column++)
+            for (int row = 0; row < 8; row++)
             {
-                for (int row = 0; row < 8; row++)
+                for (int column = 0; column < 8; column++)
                 {
-                    gameBoard[row, column] = '#';
+                    gameBoard[row, column] = new Field();
+                }
+            }
+
+            // Platziere die Minen
+            Random rnd = new Random();
+            int minesToPlace = 16;
+            while (minesToPlace > 0)
+            {
+                int x = rnd.Next(0, 8);
+                int y = rnd.Next(0, 8);
+                if (!gameBoard[x, y].IsMine())
+                {
+                    gameBoard[x, y].SetMine();
+                    minesToPlace--;
                 }
             }
 
             // Ausgabe des Spielbretts
-            Console.WriteLine(" 1  2  3  4  5  6  7  8");
-            for (int column = 0; column < 8; column++)
+            Console.WriteLine("  1   2   3   4   5   6   7   8");
+            for (int row = 0; row < 8; row++)
             {
-                Console.Write((char)('A' + column) + " ");
-                for (int row = 0; row < 8; row++)
+                Console.Write((char)('A' + row) + " ");
+                for (int column = 0; column < 8; column++)
                 {
-                    Console.Write(gameBoard[row, column] + "   ");
+                    if (gameBoard[row, column].IsRevealed())
+                    {
+                        if (gameBoard[row, column].IsMine())
+                            Console.Write("X   ");
+                        else
+                            Console.Write("O   ");
+                    }
+                    else
+                    {
+                        Console.Write("#   ");
+                    }
                 }
                 Console.WriteLine();
             }
+            // Spielerinteraktion
+            Console.WriteLine("Geben Sie die Koordinaten des Feldes ein, das Sie aufdecken möchten (z.B. A5):");
+            string input = Console.ReadLine();
+
+
+            if (input.Length == 2 && char.IsLetter(input[0]) && char.IsDigit(input[1]))
+            {
+                int column = input[0] - 'A';
+                int row = input[1] - '1';
+
+
+                if (row >= 0 && row < 8 && column >= 0 && column < 8)
+                {
+
+                    gameBoard[row, column].Reveal();
+
+                    Console.WriteLine("  1   2   3   4   5   6   7   8");
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Console.Write((char)('A' + i) + " ");
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (gameBoard[i, j].IsRevealed())
+                            {
+                                if (gameBoard[i, j].IsMine())
+                                    Console.Write("X   "); // Mienen
+                                else
+                                    Console.Write("O   "); // leere felder
+                            }
+                            else
+                            {
+                                Console.Write("#   ");//verdeckte felder
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Koordinaten. Bitte geben Sie Koordinaten innerhalb des Spielbretts ein.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ungültiges Eingabeformat. Bitte geben Sie die Koordinaten im Format 'A5' ein.");
+            }
+
+            GameBoard gameBoard2 = new GameBoard();
+
+            gameBoard2.Reveal();
         }
+
+
+
 
         public void InitFieldsMediumMode()
         {
-            char[,] gameBoard = new char[16, 16];
+            Field[,] gameBoard = new Field[16, 16];
 
-            // Fülle das Spielbrett mit #
             for (int column = 0; column < 16; column++)
             {
                 for (int row = 0; row < 16; row++)
                 {
-                    gameBoard[row, column] = '#';
+                    gameBoard[row, column] = new Field();
+                }
+            }
+
+            // Platziere die Minen
+            Random rnd = new Random();
+            int minesToPlace = 40;
+            while (minesToPlace > 0)
+            {
+                int x = rnd.Next(0, 16);
+                int y = rnd.Next(0, 16);
+                if (!gameBoard[x, y].IsMine())
+                {
+                    gameBoard[x, y].SetMine();
+                    minesToPlace--;
                 }
             }
 
@@ -94,36 +184,171 @@ namespace Minesweeper_Abel_Amadeo
                 Console.Write((char)('A' + column) + " ");
                 for (int row = 0; row < 16; row++)
                 {
-                    Console.Write(gameBoard[row, column] + "   ");
+                    if (gameBoard[row, column].IsRevealed())
+                    {
+                        if (gameBoard[row, column].IsMine())
+                            Console.Write("X   ");
+                        else
+                            Console.Write("O   ");
+                    }
+                    else
+                    {
+                        Console.Write("#   ");
+                    }
                 }
                 Console.WriteLine();
             }
+
+            // Spielerinteraktion
+            Console.WriteLine("Geben Sie die Koordinaten des Feldes ein, das Sie aufdecken möchten (z.B. A5):");
+            string input = Console.ReadLine();
+
+            if (input.Length == 2 && char.IsLetter(input[0]) && char.IsDigit(input[1]))
+            {
+                int column = input[0] - 'A';
+                int row = input[1] - '1';
+
+                if (row >= 0 && row < 16 && column >= 0 && column < 16)
+                {
+                    gameBoard[row, column].Reveal();
+
+                    Console.WriteLine("  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16");
+                    for (int i = 0; i < 16; i++)
+                    {
+                        Console.Write((char)('A' + i) + " ");
+                        for (int j = 0; j < 16; j++)
+                        {
+                            if (gameBoard[i, j].IsRevealed())
+                            {
+                                if (gameBoard[i, j].IsMine())
+                                    Console.Write("X   ");
+                                else
+                                    Console.Write("O   ");
+                            }
+                            else
+                            {
+                                Console.Write("#   ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Koordinaten. Bitte geben Sie Koordinaten innerhalb des Spielbretts ein.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ungültiges Eingabeformat. Bitte geben Sie die Koordinaten im Format 'A5' ein.");
+            }
+
+            GameBoard gameBoard2 = new GameBoard();
+
+            gameBoard2.Reveal();
         }
 
         public void InitFieldsHardMode()
         {
-            char[,] gameBoard = new char[16, 30];
+            Field[,] gameBoard = new Field[16, 30];
 
-            // Fülle das Spielbrett mit #
-            for (int column = 0; column < 30; column++)
+            for (int row = 0; row < 16; row++)
             {
-                for (int row = 0; row < 16; row++)
+                for (int column = 0; column < 30; column++)
                 {
-                    gameBoard[row, column] = '#';
+                    gameBoard[row, column] = new Field();
+                }
+            }
+
+            // Platziere die Minen
+            Random rnd = new Random();
+            int minesToPlace = 99;
+            while (minesToPlace > 0)
+            {
+                int x = rnd.Next(0, 16);
+                int y = rnd.Next(0, 30);
+                if (!gameBoard[x, y].IsMine())
+                {
+                    gameBoard[x, y].SetMine();
+                    minesToPlace--;
                 }
             }
 
             // Ausgabe des Spielbretts
-            Console.WriteLine(" 1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30");
-            for (int column = 0; column < 30; column++)
+            Console.WriteLine("  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30");
+            for (int row = 0; row < 16; row++)
             {
-                Console.Write((char)('A' + column) + " ");
-                for (int row = 0; row < 16; row++)
+                Console.Write((char)('A' + row) + " ");
+                for (int column = 0; column < 30; column++)
                 {
-                    Console.Write(gameBoard[row, column] + "   ");
+                    if (gameBoard[row, column].IsRevealed())
+                    {
+                        if (gameBoard[row, column].IsMine())
+                            Console.Write("X   ");
+                        else
+                            Console.Write("O   ");
+                    }
+                    else
+                    {
+                        Console.Write("#   ");
+                    }
                 }
                 Console.WriteLine();
+
+
+
             }
+
+            // Spielerinteraktion
+            Console.WriteLine("Geben Sie die Koordinaten des Feldes ein, das Sie aufdecken möchten (z.B. A5):");
+            string input = Console.ReadLine();
+
+            if (input.Length == 2 && char.IsLetter(input[0]) && char.IsDigit(input[1]))
+            {
+                int column = input[0] - 'A';
+                int row = input[1] - '1';
+
+                if (row >= 0 && row < 16 && column >= 0 && column < 30)
+                {
+                    gameBoard[row, column].Reveal();
+
+                    Console.WriteLine("  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30");
+                    for (int i = 0; i < 16; i++)
+                    {
+                        Console.Write((char)('A' + i) + " ");
+                        for (int j = 0; j < 30; j++)
+                        {
+                            if (gameBoard[i, j].IsRevealed())
+                            {
+                                if (gameBoard[i, j].IsMine())
+                                    Console.Write("X   ");
+                                else
+                                    Console.Write("O   ");
+                            }
+                            else
+                            {
+                                Console.Write("#   ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Koordinaten. Bitte geben Sie Koordinaten innerhalb des Spielbretts ein.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ungültiges Eingabeformat. Bitte geben Sie die Koordinaten im Format 'A5' ein.");
+            }
+
+            GameBoard gameBoard2 = new GameBoard();
+
+            gameBoard2.Reveal();
         }
+
     }
+
 }
+
--- Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/gameBoard.cs
+++ Abgabe3_Abel_Amadeo/Minesweeper_Abel_Amadeo/Minesweeper_Abel_Amadeo/gameBoard.cs
@@ -8,29 +8,81 @@ namespace Minesweeper_Abel_Amadeo
         private int width;
         private int height;
         private int mines;
-        
 
 
-        
+
+
 
         public void Reveal()
-        {
-            Menu menu = new Menu();
+{
 
-            string revealInput = Console.ReadLine();
-            if (revealInput != null )
+    Console.WriteLine("Which field do you want to reveal? Example: A5 ");
+
+    string input = Console.ReadLine();
+
+    
+    if (input.Length != 2 || !char.IsLetter(input[0]) || !char.IsDigit(input[1]))
+    {
+        Console.WriteLine("Invalid input. Please enter coordinates in the format 'LetterNumber' (e.g. A5).");
+        return;
+    }
+
+
+    int column = input[0] - 'A';
+
+  
+    int row;
+    if (!int.TryParse(input[1].ToString(), out row))
+    {
+        Console.WriteLine("Invalid input. Please enter coordinates in the format 'LetterNumber' (e.g. A5).");
+        return;
+    }
+    row--; 
+
+    
+    if (row < 0 || row >= gameBoard.GetLength(0) || column < 0 || column >= gameBoard.GetLength(1))
+    {
+        Console.WriteLine("Invalid coordinates. Please enter coordinates within the game board.");
+        return;
+    }
+
+    if (gameBoard[row, column].IsMine())
+    {
+        Console.WriteLine("GAME OVER");
+        
+    }
+    else
             {
-                Console.WriteLine("Invalid Input");
+                gameBoard[row, column].Reveal();
+
+              
+                RevealNeighbouredFields(row, column);
             }
-            else
+}
+
+
+        // Methode zum Aufdecken benachbarter Felder
+        private void RevealNeighbouredFields(int row, int column)
+        {
+            int numRows = gameBoard.GetLength(0);
+            int numColumns = gameBoard.GetLength(1);
+
+            for (int r = Math.Max(0, row - 1); r <= Math.Min(row + 1, numRows - 1); r++)
             {
-                //Aufdecken des Feldes (Felder und Mienen wurden noch nicht generiert)
+                for (int c = Math.Max(0, column - 1); c <= Math.Min(column + 1, numColumns - 1); c++)
+                {
+                    if (!gameBoard[r, c].IsRevealed())
+                    {
+                        // Feld aufdecken
+                        gameBoard[r, c].Reveal();
+                    }
+                }
             }
         }
 
         public void Flag()
         {
-        
+            //Hier muss die logik noch implementiert werden(optional)
         }
     }
 }
